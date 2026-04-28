13d10dda16ad81478a41c6080b18ce84d289aed9
luckyBambooBro <huypham008@hotmail.com>
2026-04-29 07:38:39 +0930

    started ch1L4

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luckyBambooBro/chirpy.git
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux() //type: *http.ServeMux
+	s := http.Server{ //type http.Server
+		Addr: ":8080",
+		Handler: mux,
+	}
+	s.ListenAndServe()
+}
+
