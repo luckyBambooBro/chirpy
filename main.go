5033c903c2bc878f211756b5d66c365cd878a520
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-04-30 16:17:59 +0930

    up to ch1L8 but absorbing everything else first

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const (
+		filePathRoot = "."
+		port = "8080"
+	)
 	mux := http.NewServeMux() //type: *http.ServeMux
-	s := http.Server{ //type http.Server
-		Addr: ":8080",
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	
+	srv := &http.Server{ //type http.Server
+		Addr: ":" + port,
 		Handler: mux,
 	}
-	s.ListenAndServe()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	log.Printf("Serving files from %s on port:%s\n", filePathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 
 }
 
