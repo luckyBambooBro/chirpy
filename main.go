096c8e7c63fe2babeaac80e6508111a21e6e174f
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-05-05 17:07:07 +0930

    started next lesson

--- main.go
+++ main.go
@@ -12,11 +12,18 @@ func main() {
 	)
 	mux := http.NewServeMux() //type: *http.ServeMux
 	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
-	
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK")) //converting string "OK" to a slice of bytes
+	})
+
 	srv := &http.Server{ //type http.Server
 		Addr: ":" + port,
 		Handler: mux,
 	}
 	log.Printf("Serving files from %s on port:%s\n", filePathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+	//anything after this line will not work as the previous line blocks
+
 }
