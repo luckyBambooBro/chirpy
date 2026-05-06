7929412e1663db58d47c2a16521525d058209699
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-05-06 16:12:55 +0930

    up to last lesson of ch1

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 		port = "8080"
 	)
 	mux := http.NewServeMux() //type: *http.ServeMux
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
