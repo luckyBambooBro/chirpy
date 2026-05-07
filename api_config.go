1c6c3b8574e140ddb44d90cd01b56e3d26229f78
luckyBambooBro <huypham008@hotmail.com>
2026-05-07 15:47:07 +0930

    still fixing ch2L2

--- api_config.go
+++ api_config.go
@@ -11,9 +11,14 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (cfg *apiConfig) apiHandler() {
-	fmt.Printf("Hits: %v", cfg.fileserverHits)
+func (cfg *apiConfig) apiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
 }
