22477566f152ea676a6ff3ece6dad40c78103ef7
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-05-09 12:21:17 +0930

    ready to move onto ch2L3

--- main.go
+++ main.go
@@ -26,9 +26,5 @@ func main() {
 	//anything after this line will not work as the previous line blocks
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK))) 
-}
+
 
