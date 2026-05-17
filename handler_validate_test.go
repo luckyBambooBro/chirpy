e5368f50b43664ccf8a4490884ca9706ec02c997
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-05-17 17:19:46 +0930

    done unit tests

--- handler_validate_test.go
+++ handler_validate_test.go
@@ -22,25 +22,26 @@ func TestHandlerChirpsValidate(t *testing.T) {
 			name: "standard string",
 			inputBody: `{"body": "whatever trevor dawg"}`,
 			expectedStatus: http.StatusOK,
-			expectedJSON: `{"cleaned_body": "whatever trevor dawg"}`,
+			expectedJSON: `{"cleaned_body":"whatever trevor dawg"}`,
 		},
 			{
 			name: "standard string",
 			inputBody: `{"body": "what in the kerfuffle is that!"}`,
 			expectedStatus: http.StatusOK,
-			expectedJSON: `{"cleaned_body": "what in the **** is that!"}`,
+			expectedJSON: `{"cleaned_body":"what in the **** is that!"}`,
 		},
 	}
 
 	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T){
 			r := httptest.NewRequestWithContext(
-				context.Background(), 
-				http.MethodPost, 
-				"/api/validate_chirp",
-				strings.NewReader(tc.inputBody),
-				)
-			w := httptest.NewRecorder()
-		handlerChirpsValidate(w, r)
+			context.Background(), 
+			http.MethodPost, 
+			"/api/validate_chirp",
+			strings.NewReader(tc.inputBody),
+			)
+		w := httptest.NewRecorder()
+			handlerChirpsValidate(w, r)
 		resp := w.Result()
 		if resp.StatusCode != http.StatusOK {
 			t.Errorf("expected status: %v\nactual status: %v\n", http.StatusOK, resp.StatusCode)
@@ -56,5 +57,7 @@ func TestHandlerChirpsValidate(t *testing.T) {
 		if actualStr != expectedStr {
 			t.Errorf("JSON body mismatch\nactual: %v, expected: %v", actualStr, expectedStr)
 		}
+		})	
+
 	}
 }
