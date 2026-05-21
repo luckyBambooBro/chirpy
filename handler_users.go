4ca1fd49fc9a61188eb93305c6cb596181334bba
luckyBambooBro <huypham008@hotmail.com>
2026-05-22 06:42:57 +0930

    adding primary key to schema to fix it. revert here to access code prior to change

--- handler_users.go
+++ handler_users.go
@@ -4,25 +4,65 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/luckyBambooBro/chirpy/internal/database"
 )
 
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
 func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
 	type userData struct{
-		userEmail string
+		Email string `json:"email"`
 	}
 	
 	decoder := json.NewDecoder(r.Body)
 	defer r.Body.Close()
 	requestData := &userData{}
 	if err := decoder.Decode(requestData); err != nil {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Server Error"))
+
 		log.Printf("error decoding request to create user: %v", err)
+		returnErrorMsg(w)
 		return
 	}
-	//come back to the following after decoding the resp
-	cfg.db.CreateUser(r.Context(), requestData.userEmail)
+	//create user
+	createUser, err := cfg.db.CreateUser(r.Context(), requestData.Email)
+	if err != nil {
+		log.Printf("error creating user: %v", err)
+		returnErrorMsg(w)
+	}
+
+	user := User{
+		ID: createUser.ID,
+		CreatedAt: createUser.CreatedAt,
+		UpdatedAt: createUser.UpdatedAt,
+		Email: createUser.Email,
+	}
+
+	data, err := json.Marshal(user)
+	if err != nil {
+		log.Printf("error encoding user data: %v", err)
+	}
+
+	//return data in json object
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if _, err = w.Write(data); err != nil {
+		log.Println(err)
+	}
+
+}
+
+func returnErrorMsg(w http.ResponseWriter) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusInternalServerError)
+		if _, err := w.Write([]byte("Server Error")); err != nil {
+			log.Println(err)
+		}
 }
