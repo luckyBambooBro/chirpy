759555638ba3edadd93ec1847a6899359cf5f0b2
luckyBambooBro <huypham008@hotmail.com>
2026-06-01 20:15:42 +0930

    redo jwt_test.go same way as the lesson did before moving on

--- internal/auth/jwt_test.go
+++ internal/auth/jwt_test.go
@@ -9,6 +9,7 @@ import (
 
 func TestJWT(t *testing.T) {
 	correctSecret, incorrectSecret := "asad-yrtv-regt-felg-jvwn", "thisIsTheWrongSecretToken"
+	userID := uuid.New()
 
 	type testUnit struct {
 		Name string
@@ -20,21 +21,21 @@ func TestJWT(t *testing.T) {
 	correctCases := []testUnit{
 		{
 		Name: "General Test",
-		Uuid: uuid.New(),
+		Uuid: userID,
 		TokenSecret: correctSecret,
 		ExpireLimit: 3 * time.Second,
 		},
 		{
 		Name: "Typed Out Secret Token Test",
-		Uuid: uuid.New(),
+		Uuid: userID,
 		TokenSecret: "asad-yrtv-regt-felg-jvwn",
 		ExpireLimit: 5 * time.Second,
 		},
 	}
 
-	for _, testCase := range correctCases {
-		t.Run(testCase.Name, func(t *testing.T) {
-			jwt, err := MakeJWT(testCase.Uuid, testCase.TokenSecret, testCase.ExpireLimit)
+	for _, tt := range correctCases {
+		t.Run(tt.Name, func(t *testing.T) {
+			jwt, err := MakeJWT(tt.Uuid, tt.TokenSecret, tt.ExpireLimit)
 			if err != nil {
 				t.Fatalf("error creating jwt: %q", err)
 			}
@@ -49,28 +50,28 @@ func TestJWT(t *testing.T) {
 	incorrectCases := []testUnit {
 		{
 		Name: "Incorrect Secret Test",
-		Uuid: uuid.New(),
+		Uuid: userID,
 		TokenSecret: incorrectSecret,
 		ExpireLimit: 10 * time.Second,
 		},
 		{
 		Name: "Expired Test",
-		Uuid: uuid.New(),
+		Uuid: userID,
 		TokenSecret: correctSecret,
 		ExpireLimit: -1 * time.Second,
 		},
 		{
 		Name: "Empty Secret",
-		Uuid: uuid.New(),
+		Uuid: userID,
 		TokenSecret: "",
 		ExpireLimit: 3 * time.Second,
 		},
 		
 	}
 
-	for _, testCase := range incorrectCases {
-		t.Run(testCase.Name, func(t *testing.T) {
-			jwt, err := MakeJWT(testCase.Uuid, testCase.TokenSecret, testCase.ExpireLimit)
+	for _, tt := range incorrectCases {
+		t.Run(tt.Name, func(t *testing.T) {
+			jwt, err := MakeJWT(tt.Uuid, tt.TokenSecret, tt.ExpireLimit)
 			if err != nil {
 				t.Fatal("unable to create jwt")
 			}
